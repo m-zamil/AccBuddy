4217a87e395b0dd684f33798305c55737ad2f710
Zamil <muzamil-javascriptdev@outlook.com>
2024-04-29 15:31:30 +0500

    first commit, header completed

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('[data-hamburger-menu-open]');
+  const closeHamb = document.querySelector('[data-hamburger-menu-close]');
+
+  const headerNav = document.querySelector('[data-header-nav]');
+
+  const logo = document.querySelector('[data-logo]');
+  console.log(headerNav);
+
+  const topbar = document.querySelector('[data-header-mobile-topbar');
+  const bottomBar = document.querySelector('[data-header-bottom-bar');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.style.display = 'none';
+    closeHamb.style.display = 'inline-block';
+    headerNav.style.display = 'block';
+  });
+  closeHamb.addEventListener('click', () => {
+    hamburger.style.display = 'inline-block';
+    closeHamb.style.display = 'none';
+    headerNav.style.display = 'none';
+  });
+});
