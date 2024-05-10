1e3704d8fbb35844922256cccfda3a4a5f035f98
Zamil <muzamil-javascriptdev@outlook.com>
2024-05-11 00:26:00 +0500

    updated with some fixes and write reviews styles

--- assets/js/product-card.js
+++ assets/js/product-card.js
@@ -1,6 +1,6 @@
 const incBtn = document.querySelector('.inc-btn');
 const decBtn = document.querySelector('.dec-btn');
-const quantity = document.querySelector('#quantity');
+const quantity = document.querySelector('.quantity');
 
 incBtn.addEventListener('click', () => {
   quantity.value = Number(quantity.value) + 1;
@@ -9,4 +9,12 @@ decBtn.addEventListener('click', () => {
   if (quantity.value > 0) {
     quantity.value = Number(quantity.value) - 1;
   }
+});
+
+const localLinks = document.querySelectorAll("a[href ^= '#']");
+localLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector(`[data-target=${link.hash.split('#')[1]}]`).scrollIntoView();
+  });
 });
